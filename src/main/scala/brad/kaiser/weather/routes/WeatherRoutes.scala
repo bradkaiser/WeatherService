634c7b9a549b5776f4dedfe7d07e502af03a42ed
Brad Kaiser <brad.kaiser@vonage.com>
2021-10-31 02:49:26 -0400

    cleanup

--- src/main/scala/brad/kaiser/weather/routes/WeatherRoutes.scala
+++ src/main/scala/brad/kaiser/weather/routes/WeatherRoutes.scala
@@ -1,35 +1,27 @@
 package brad.kaiser.weather.routes
 
-import brad.kaiser.weather.models.{OpenWeatherResponse, WeatherServiceResponse}
+import brad.kaiser.weather.models.WeatherServiceResponse
 import brad.kaiser.weather.services.OpenWeatherService
 import cats.Functor
 import cats.effect._
-import cats.syntax.all._
 import org.http4s._
 import org.http4s.dsl.io._
-import org.http4s.implicits._
-import org.http4s.circe.CirceEntityCodec._
 import io.circe.generic.auto._
-import org.http4s.circe.jsonOf
 import org.http4s.circe.jsonEncoderOf
 import org.http4s.dsl.Http4sDsl
 
-
+/**
+ * Routes for querying the weather.
+ */
 object WeatherRoutes {
   object latParam extends QueryParamDecoderMatcher[Double]("lat")
   object lonParam extends QueryParamDecoderMatcher[Double]("lon")
-  implicit def decoder[F[_]: Concurrent]: EntityDecoder[F, WeatherServiceResponse] = jsonOf[F, WeatherServiceResponse]
   implicit def encoder[F[_]]: EntityEncoder[F, WeatherServiceResponse] = jsonEncoderOf[F, WeatherServiceResponse]
 
   def routes[F[_]: Async: Functor](openWeatherService: OpenWeatherService[F]): HttpRoutes[F] = {
     object dsl extends Http4sDsl[F]; import dsl._
     HttpRoutes.of[F] {
-      //case GET -> Root / "weather" :? latParam(lat)  +& lonParam(lon)  => openWeatherService.query(lat, lon).flatMap(Ok(_))
-      //    case GET -> Root / "weather" :? latParam(lat)  +& lonParam(lon)  => Ok(openWeatherService.query(lat, lon))
-      case GET -> Root / "weather" :? latParam(lat)  +& lonParam(lon)  => for {
-        result <- openWeatherService.query(lat, lon)
-        response <- Ok(result)
-      } yield response
+      case GET -> Root / "weather" :? latParam(lat)  +& lonParam(lon)  => Ok(openWeatherService.query(lat, lon))
     }
   }
 }
