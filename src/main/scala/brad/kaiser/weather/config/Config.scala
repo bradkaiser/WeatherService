a4f739556583022c972783bcd02f598e6c280387
Brad Kaiser <brad.kaiser@vonage.com>
2021-10-31 02:44:34 -0400

    cleanup

--- src/main/scala/brad/kaiser/weather/config/Config.scala
+++ src/main/scala/brad/kaiser/weather/config/Config.scala
@@ -1,3 +1,7 @@
 package brad.kaiser.weather.config
 
+/**
+ * Describe config files
+ */
 case class Config(openWeather: OpenWeatherConfig)
+case class OpenWeatherConfig (url: String, apiKey: String)
--- src/main/scala/brad/kaiser/weather/config/OpenWeatherConfig.scala
+++
@@ -1,4 +0,0 @@
-package brad.kaiser.weather.config
-
-case class OpenWeatherConfig (url: String,
-                              apiKey: String)
