7a6cdce528230f34efe10183f8ec12ab105da10e
Brad Kaiser <brad.kaiser@vonage.com>
2021-10-31 02:42:34 -0400

    cleanup

--- src/main/scala/brad/kaiser/weather/Main.scala
+++ src/main/scala/brad/kaiser/weather/Main.scala
@@ -3,39 +3,22 @@ package brad.kaiser.weather
 import brad.kaiser.weather.config.Config
 import brad.kaiser.weather.routes.WeatherRoutes
 import brad.kaiser.weather.services.OpenWeatherService
-import cats.syntax.all._
 import cats.effect._
 import org.http4s.blaze.server._
 import org.http4s.implicits._
-import org.http4s._
 import org.http4s.blaze.client.BlazeClientBuilder
-import org.http4s.dsl.io._
-import org.http4s.server.{Router, Server}
+import org.http4s.server.Router
 import pureconfig._
 import pureconfig.generic.auto._
 
-
-
-import scala.concurrent.ExecutionContext.Implicits.global
-
-/* Todo
-rename application.conf for defaults
-move client out of request
-add more tests
-add logs
-rearrange initializations
- */
-
 object Main extends IOApp {
-
   def run(args: List[String]): IO[ExitCode] = {
-    println("hello")
     val config: Config = ConfigSource.default.load[Config].getOrElse(throw new IllegalArgumentException("died"))
 
     val server = for {
       client <- BlazeClientBuilder[IO].resource
       openWeatherService = OpenWeatherService(config.openWeather, client)
-      router = Router("/" -> WeatherRoutes.routes(openWeatherService)).orNotFound
+      router = Router("" -> WeatherRoutes.routes(openWeatherService)).orNotFound
       server <- BlazeServerBuilder[IO]
           .bindHttp(8080, "localhost")
           .withHttpApp(router)
@@ -43,21 +26,5 @@ object Main extends IOApp {
     } yield server
 
     server.use(_ => IO.never).as(ExitCode.Success)
-
-
-    //    val openWeatherService = OpenWeatherService(config.openWeather)
-//
-//    println(config)
-//
-//
-//    val router = Router("/" -> WeatherRoutes.routes(openWeatherService)).orNotFound
-
-//    BlazeServerBuilder[IO]
-//      .bindHttp(8080, "localhost")
-//      .withHttpApp(router)
-//      .serve
-//      .compile
-//      .drain
-//      .as(ExitCode.Success)
   }
 }
